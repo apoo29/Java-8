93fde809379ca33190462368f8ee484551f6a40e
apoupadh <apoorvaimi379@gmail.com>
2022-04-18 17:20:12 +0530

    java changes

---
+++ src/com/java/streams_terminal/StreamsSumAvgExample.java
@@ -0,0 +1,36 @@
+package com.java.streams_terminal;
+
+import com.java.data.Student;
+import com.java.data.StudentDataBase;
+
+import java.util.stream.Collectors;
+
+public class StreamsSumAvgExample {
+
+    public static int sum(){
+
+        int totalNoOfNotebooks = StudentDataBase.getAllStudents()
+                .stream()
+                .collect(Collectors.summingInt(Student::getNoteBooks));
+
+        return totalNoOfNotebooks;
+    }
+
+    public static double average(){
+
+        double totalNoOfNotebooks = StudentDataBase.getAllStudents()
+                .stream()
+                .collect(Collectors.averagingInt(Student::getNoteBooks));
+
+        return totalNoOfNotebooks;
+    }
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Total no of Notebooks : " + sum());
+
+        System.out.println("Average no of Notebooks : " + average());
+    }
+}
